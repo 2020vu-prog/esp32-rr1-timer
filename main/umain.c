935837b26f84f79455d4ed479cd9a28b61b020fb
Chris Witte <chris.witte@extcare.com>
2026-02-26 11:51:16 -0600

    chug

--- main/umain.c
+++ main/umain.c
@@ -68,7 +68,7 @@ void app_main(void)
 
 
 temperature_sensor_handle_t temp_handle = NULL;
-temperature_sensor_config_t temp_sensor_config = TEMPERATURE_SENSOR_CONFIG_DEFAULT(20, 50);
+temperature_sensor_config_t temp_sensor_config = TEMPERATURE_SENSOR_CONFIG_DEFAULT(20, 80);
 ESP_ERROR_CHECK(temperature_sensor_install(&temp_sensor_config, &temp_handle));
 
 
