fba7592fcf2a950b5101a0d4bf4576db638ef9f1
Chris Witte <cwitte@xmlhq.com>
2026-03-26 17:40:37 -0500

    github include case sensitive?

--- components/rr1_messages/aws_bandaid.c
+++ components/rr1_messages/aws_bandaid.c
@@ -1,35 +1,37 @@
 #include "mbedtls/base64.h"
 #include "esp_heap_caps.h"
-#include <cjson.h>
+// #include <cjson.h>
+#include <cJSON.h>
+// #include <cJSON/cJSON.h>
 
 #include <string.h>
 
 static int seq = 0;
 char *aba_b64_json(char *buffer)
 {
-	char *string = NULL;
+    char *string = NULL;
 
-	cJSON *monitor = cJSON_CreateObject();
-	if (monitor == NULL)
-	{
-		goto end;
-	}
+    cJSON *monitor = cJSON_CreateObject();
+    if (monitor == NULL)
+    {
+        goto end;
+    }
 
-	if (cJSON_AddNumberToObject(monitor, "seq", seq++) == NULL)
-	{
-		goto end;
-	}
-	if (cJSON_AddStringToObject(monitor, "b64", buffer) == NULL)
-	{
-		goto end;
-	}
-	string = cJSON_PrintUnformatted(monitor);
-	if (string == NULL)
-	{
-		fprintf(stderr, "Failed to print monitor.\n");
-	}
+    if (cJSON_AddNumberToObject(monitor, "seq", seq++) == NULL)
+    {
+        goto end;
+    }
+    if (cJSON_AddStringToObject(monitor, "b64", buffer) == NULL)
+    {
+        goto end;
+    }
+    string = cJSON_PrintUnformatted(monitor);
+    if (string == NULL)
+    {
+        fprintf(stderr, "Failed to print monitor.\n");
+    }
 
 end:
-	cJSON_Delete(monitor);
-	return string;
+    cJSON_Delete(monitor);
+    return string;
 }
