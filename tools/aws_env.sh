e6a778b4452190c95b7df5d899c93ecab22456dc
Chris Witte <cwitte@xmlhq.com>
2026-05-18 07:28:11 -0500

    shellBucket

--- tools/aws_env.sh
+++ tools/aws_env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo $BRANCH_NAME   
 export AWS_ACCOUNT=''
+export AWS_BUCKET=''
 if [[ "$BRANCH_NAME" == "main" ]]; then
         export AWS_ACCOUNT='1.2.3'  
            
@@ -9,6 +10,8 @@ fi
 
 if [[ "$BRANCH_NAME" == "dev" ]]; then
         export AWS_ACCOUNT='178616746534'  
+        export AWS_BUCKET='svelte-static-20221204164305433000000002'
 
 fi
  echo "AWS_ACCOUNT=$AWS_ACCOUNT" >> $GITHUB_ENV
+ echo "AWS_BUCKET=$AWS_BUCKET" >> $GITHUB_ENV
