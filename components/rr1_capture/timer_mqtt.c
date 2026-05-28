570cd9b3fa62bb729a4c3375f7b640a62bfd10f4
Chris Witte <cwitte@xmlhq.com>
2026-05-28 10:25:39 -0500

    Nmea2 (#18)
    * secondsTroubleshoot
    
    * chug

--- components/rr1_capture/timer_mqtt.c
+++ components/rr1_capture/timer_mqtt.c
@@ -95,7 +95,8 @@ void time_sync_notification_cb(struct timeval *tv) {
 static void initialize_sntp(void) {
   ESP_LOGI(TAG, "Initializing SNTP");
   esp_sntp_setoperatingmode(SNTP_OPMODE_POLL);
-  esp_sntp_setservername(0, "pool.ntp.org");
+  esp_sntp_set_sync_mode(SNTP_SYNC_MODE_IMMED);
+  esp_sntp_setservername(0, "1.us.pool.ntp.org");
   esp_sntp_set_time_sync_notification_cb(time_sync_notification_cb);
   esp_sntp_init();
   ESP_LOGI(TAG, "SNTP initialization done.");
